6cde845b0223d80e2698b286d63689cbfe0e3d3d
Abhay-PVI <abhaypratap.singh@pvinsight.com>
2026-08-03 12:20:18 +0530

    chore: update base path in vite.config.js to support environment variable configuration and ignore pycache files

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-   base: './'   // ✅ very important for shared drive
+  base: process.env.VITE_BASE || '/',
 })
